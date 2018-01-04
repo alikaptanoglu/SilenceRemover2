adc9afebe6f8d65e49358b4fe22a43c81e949bde
Emile Akbarzadeh <emile.aks@gmail.com>
2018-01-04 18:27:41 +1100

    Major release 1.0

--- SilenceRemover2/Form1.Designer.cs
+++ SilenceRemover2/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.textBlockBoye = new System.Windows.Forms.Label();
+            this.processButton = new System.Windows.Forms.Button();
+            this.fileSaveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.decibelPicker = new System.Windows.Forms.NumericUpDown();
+            this.Preview = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.decibelPicker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(13, 13);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(130, 35);
+            this.fileButton.TabIndex = 0;
+            this.fileButton.Text = "Choose input";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // textBlockBoye
+            // 
+            this.textBlockBoye.AutoSize = true;
+            this.textBlockBoye.Location = new System.Drawing.Point(97, 116);
+            this.textBlockBoye.Name = "textBlockBoye";
+            this.textBlockBoye.Size = new System.Drawing.Size(49, 17);
+            this.textBlockBoye.TabIndex = 1;
+            this.textBlockBoye.Text = "Ready";
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(499, 12);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(111, 79);
+            this.processButton.TabIndex = 2;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // fileSaveButton
+            // 
+            this.fileSaveButton.Location = new System.Drawing.Point(13, 54);
+            this.fileSaveButton.Name = "fileSaveButton";
+            this.fileSaveButton.Size = new System.Drawing.Size(130, 37);
+            this.fileSaveButton.TabIndex = 3;
+            this.fileSaveButton.Text = "Choose output";
+            this.fileSaveButton.UseVisualStyleBackColor = true;
+            this.fileSaveButton.Click += new System.EventHandler(this.fileSaveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Minimum decibels";
+            // 
+            // decibelPicker
+            // 
+            this.decibelPicker.Location = new System.Drawing.Point(353, 316);
+            this.decibelPicker.Name = "decibelPicker";
+            this.decibelPicker.Size = new System.Drawing.Size(120, 22);
+            this.decibelPicker.TabIndex = 6;
+            this.decibelPicker.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.decibelPicker.ValueChanged += new System.EventHandler(this.decibelValue_Changed);
+            // 
+            // Preview
+            // 
+            this.Preview.Location = new System.Drawing.Point(499, 116);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(111, 79);
+            this.Preview.TabIndex = 7;
+            this.Preview.Text = "Preview";
+            this.Preview.UseVisualStyleBackColor = true;
+            this.Preview.Click += new System.EventHandler(this.previewButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(100, 316);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sample length (seconds)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 409);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Preview);
+            this.Controls.Add(this.decibelPicker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileSaveButton);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.textBlockBoye);
+            this.Controls.Add(this.fileButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.decibelPicker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.Label textBlockBoye;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.Button fileSaveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown decibelPicker;
+        private System.Windows.Forms.Button Preview;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- SilenceRemover2/Form1.cs
+++ SilenceRemover2/Form1.cs
@@ -7,14 +7,326 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using NAudio;
+
+//NEXT:
+/* DONE-Cut up video according to silencedict
+ * DONE-Concat video again
+ * DONE-slider controls
+ * DB meter
+ * Add preview
+ * clean ui
+ * add video player
+ * */
 
 namespace SilenceRemover2
 {
     public partial class Form1 : Form
     {
+        private string inputVidLocation = "";
+        private string outputVidLocation = "";
+        private List<string> tempFiles = new List<string>();
+        private int currentTempIndex = -1;
+        private float minDecibel = 90;
+        private float sampleLength = 1;
+        private Dictionary<int, bool> silenceDict = new Dictionary<int, bool>();
+        private bool currConverting = false;
+        private bool currPreviewing = false;
+
         public Form1()
         {
             InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            //clean up temp files
+            foreach (string tempLoc in tempFiles)
+            {
+                try
+                {
+                    File.Delete(tempLoc);
+                }
+                catch { }
+            }
+        }
+
+        private void decibelValue_Changed(object sender, EventArgs e)
+        {
+            NumericUpDown decibelPicker = sender as NumericUpDown;
+            minDecibel = Convert.ToInt32(decibelPicker.Value);
+        }
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            handleFilePicker();
+        }
+
+        private void fileSaveButton_Click(object sender, EventArgs e)
+        {
+            handleFileSaver();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            sampleLength = (float)((sender as NumericUpDown).Value);
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (inputVidLocation == null)
+            {
+                textBlockBoye.Text = "Please choose a file first";
+            }
+            else
+            {
+                currConverting = true;
+                //start processing
+                textBlockBoye.Text = "Extracting audio...";
+                var tsk = Task<int>.Factory.StartNew(() => audioExtractTask()).ContinueWith(a => audioParseCompleted(),
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+
+        private void previewButton_Click(object sender, EventArgs e)
+        {
+            if (inputVidLocation == null)
+            {
+                textBlockBoye.Text = "Please choose a file first";
+            }
+            else
+            {
+                currPreviewing = true;
+                //start processing
+                textBlockBoye.Text = "Extracting audio...";
+                var tsk = Task<int>.Factory.StartNew(() => audioExtractTask()).ContinueWith(a => audioParseCompleted(),
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+
+        private void handleFilePicker()
+        {
+            var picker = new OpenFileDialog();
+            picker.Filter = "mp4 file (*.mp4)|*.mp4|All files (*.*)|*.*";
+            picker.FilterIndex = 1;
+            picker.Title = "Select the video file";
+            picker.Multiselect = false;
+            if (picker.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    inputVidLocation = picker.FileName;
+                    textBlockBoye.Text = inputVidLocation;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void handleFileSaver()
+        {
+            var picker = new SaveFileDialog();
+            picker.Filter = "mp4 file (*.mp4)|*.mp4";
+            picker.Title = "Save the video file";
+            if (picker.ShowDialog() == DialogResult.OK && picker.FileName != "")
+            {
+                try
+                {
+                    outputVidLocation = picker.FileName;
+                    textBlockBoye.Text = outputVidLocation;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not save file to disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        //processing from here onwards:
+        private int audioExtractTask()
+        {
+            tempFiles.Add(Path.GetTempFileName());
+            currentTempIndex++;
+            if (File.Exists(tempFiles[currentTempIndex]))
+            {
+                File.Delete(tempFiles[currentTempIndex]);
+            }
+            var ffmpegProcess = new Process();
+            ffmpegProcess.StartInfo.UseShellExecute = false;
+            //ffmpegProcess.StartInfo.RedirectStandardInput = true;
+            //ffmpegProcess.StartInfo.RedirectStandardOutput = true;
+            //ffmpegProcess.StartInfo.RedirectStandardError = true;
+            ffmpegProcess.StartInfo.CreateNoWindow = true;
+            ffmpegProcess.StartInfo.FileName = "ffmpeg/ffmpeg.exe";
+            ffmpegProcess.StartInfo.Arguments = " -i " + inputVidLocation + " -vn -f wav -ar 48000 " + tempFiles[currentTempIndex];
+            ffmpegProcess.Start();
+            //ffmpegProcess.StandardOutput.ReadToEnd();
+            //ffmpegProcess.StandardError.ReadToEnd();
+            ffmpegProcess.WaitForExit();
+            if (!ffmpegProcess.HasExited)
+            {
+                ffmpegProcess.Kill();
+            }
+            return 1;
+        }
+
+        private int audioParseCompleted()
+        {
+            //now process audio
+            //textBlockBoye.Text = "Processing audio...";
+            textBlockBoye.Text = tempFiles[currentTempIndex];
+            var tsk = Task<int>.Factory.StartNew(() => audioProcessTask()).ContinueWith(a => audioProcessCompleted(),
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            return 1;
+        }
+
+        //audio processing
+        private int audioProcessTask()
+        {
+            silenceDict.Clear();
+            using (NAudio.Wave.AudioFileReader wave = new NAudio.Wave.AudioFileReader(tempFiles[currentTempIndex]))
+            {
+                int samplesPerSecond = Convert.ToInt32(Math.Round(wave.WaveFormat.SampleRate * wave.WaveFormat.Channels * sampleLength)); ;
+                var readBuffer = new float[samplesPerSecond];
+                int samplesRead;
+                int i = 1;
+                do
+                {
+                    samplesRead = wave.Read(readBuffer, 0, samplesPerSecond);
+                    if (samplesRead == 0) break;
+                    var max = readBuffer.Take(samplesRead).Max();
+                    if ((int)(max * 100) > minDecibel)
+                        silenceDict.Add(i, false);
+                    else
+                        silenceDict.Add(i, true);
+
+                    i++;
+                } while (samplesRead > 0);
+            }
+            return 1;
+        }
+
+        private int audioProcessCompleted()
+        {
+            //now process audio
+            int trues = 0;
+            for (int i = 1; i <= silenceDict.Count; i++)
+            {
+                if (silenceDict[i]) { trues++; }
+            }
+            textBlockBoye.Text = "There are " + Convert.ToString(trues * sampleLength) + " seconds of silence";
+            if (currConverting)
+            {
+                var tsk = Task<int>.Factory.StartNew(() => videoCutTask()).ContinueWith(a => videoCutComplete(),
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            if (currPreviewing)
+            {
+                var tsk = Task<int>.Factory.StartNew(() => previewTask()).ContinueWith(a => previewTaskComplete(),
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            return 1;
+        }
+
+        private int videoCutTask()
+        {
+            var startTime = new List<float>();
+            var endTime = new List<float>();
+            bool wasSilent = true;
+
+            for (int i = 1; i <= silenceDict.Count; i++)
+            {
+                if (!silenceDict[i])
+                {
+                    if (wasSilent)
+                    {
+                        startTime.Add((i - 1) * sampleLength);
+                    }
+                    wasSilent = false;
+                }
+                else if (silenceDict[i])
+                {
+                    if (!wasSilent)
+                    {
+                        endTime.Add((i - 1) * sampleLength);
+                    }
+                    wasSilent = true;
+                }
+            }
+            //now cut the video
+
+            int originalTempIndex = currentTempIndex;
+            string inputString = "";
+            for (int i = 0; i < startTime.Count; i++)
+            {
+                tempFiles.Add(Path.GetTempPath() + Guid.NewGuid().ToString() + ".mp4");
+                currentTempIndex++;
+                //ffmpeg
+                inputString = " -ss " + startTime[i] + " -i " + inputVidLocation + " -t " + Convert.ToString(endTime[i] - startTime[i]) + " -c copy " + tempFiles[currentTempIndex];
+
+                var ffmpegProcess = new Process();
+                ffmpegProcess.StartInfo.UseShellExecute = false;
+                ffmpegProcess.StartInfo.CreateNoWindow = true;
+                ffmpegProcess.StartInfo.FileName = "ffmpeg/ffmpeg.exe";
+                ffmpegProcess.StartInfo.Arguments = inputString;
+                ffmpegProcess.Start();
+                ffmpegProcess.WaitForExit();
+                if (!ffmpegProcess.HasExited)
+                {
+                    ffmpegProcess.Kill();
+                }
+            }
+            //making input text file list
+            tempFiles.Add(Path.GetTempFileName());
+            currentTempIndex++;
+            using (StreamWriter sw = File.CreateText(tempFiles[currentTempIndex]))
+            {
+                for (int i = originalTempIndex + 1; i < currentTempIndex; i++)
+                {
+                    sw.WriteLine("file '{0}'", tempFiles[i]);
+                }
+                sw.Close();
+            }
+            //concatenating
+            inputString = "-f concat -safe 0 -i " + tempFiles[currentTempIndex] + " -c copy " + outputVidLocation;
+            var ffmpegP = new Process();
+            ffmpegP.StartInfo.UseShellExecute = false;
+            ffmpegP.StartInfo.CreateNoWindow = true;
+            ffmpegP.StartInfo.FileName = "ffmpeg/ffmpeg.exe";
+            ffmpegP.StartInfo.Arguments = inputString;
+            ffmpegP.Start();
+            ffmpegP.WaitForExit();
+            if (!ffmpegP.HasExited)
+            {
+                ffmpegP.Kill();
+            }
+            return 1;
+        }
+
+        private int videoCutComplete()
+        {
+            textBlockBoye.Text = "Succesfully finished!";
+            return 1;
+        }
+
+        private int previewTask()
+        {
+            return 1;
+        }
+
+        private int previewTaskComplete()
+        {
+            //textBlockBoye.Text = "Succesfully generated preview!";
+            return 1;
         }
     }
 }
